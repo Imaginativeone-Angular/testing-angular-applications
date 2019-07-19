45b56d0d02d03d55ab04d50e10808faab1d97aa8
Doug Franklin <doug@dougfranklin.com>
2019-07-19 07:40:03 -0400

    Testing File Shell

---
+++ src/app/contacts/contacts.component.spec.ts
@@ -0,0 +1,12 @@
+import { ContactsComponent } from '../contacts/contacts.component';
+// import { Contact } from '../contacts/shared/models';
+
+// describe('ContactsComponent Tests', () => {
+
+//     it('Should be a basic testing shell', () => {
+
+//         expect(true).toBe(true);
+
+//     });
+
+// });
