5c8f64bd8081f6f247c001289cb3e1b4295a1ae5
Doug Franklin <doug@dougfranklin.com>
2019-07-19 07:30:28 -0400

    Initial Commit

---
+++ src/app/contacts/shared/services/contact.service.ts
@@ -0,0 +1,70 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { map } from 'rxjs/operators';
+
+import { Contact } from '../';
+
+@Injectable()
+export class ContactService {
+  private contactsUrl = 'app/contacts';
+  private headers: HttpHeaders = new HttpHeaders({'Content-Type': 'application/json'});
+
+  constructor(private http: HttpClient) { }
+
+  public getContacts(): any {
+    return this.http.get(this.contactsUrl)
+      .pipe(map((next: any) => next.data))
+      .toPromise()
+      .catch(this.handleError);
+  }
+
+  public getContact(id: number): Promise<Contact> {
+    return this.getContacts()
+      .then(contacts => contacts.find(contact => contact.id === id));
+  }
+
+  public save(contact: Contact): Promise<Contact> {
+    if (contact.id) {
+      return this.put(contact);
+    }
+
+    return this.post(contact);
+  }
+
+  public new(contact: Contact): Promise<Contact> {
+    return this.post(contact);
+  }
+
+  public delete(contact: Contact): Promise<Contact> {
+    const url = `${this.contactsUrl}/${contact.id}`;
+
+    return this.http
+      .delete(url, {headers: this.headers})
+      .toPromise()
+      .then(() => null)
+      .catch(this.handleError);
+  }
+
+  public post(contact: Contact): Promise<Contact> {
+    return this.http
+      .post(this.contactsUrl, contact, {headers: this.headers})
+      .toPromise()
+      .catch(this.handleError);
+  }
+
+  public put(contact: Contact): Promise<Contact> {
+    const url = `${this.contactsUrl}/${contact.id}`;
+
+    return this.http
+      .put(url, contact, {headers: this.headers})
+      .toPromise()
+      .then(() => contact)
+      .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error);
+    return Promise.reject(error.message || error);
+  }
+}
