e080a3db837b5832a2adfa57e2616c549f4f0dfb
Doug Franklin <doug@dougfranklin.com>
2019-07-19 07:47:00 -0400

    Null ContactsComponent Instance Added

--- src/app/contacts/contacts.component.spec.ts
+++ src/app/contacts/contacts.component.spec.ts
@@ -3,10 +3,14 @@ import { Contact } from '../contacts/shared/models';
 
 describe('ContactsComponent Tests', () => {
 
+    let ContactsComponent: ContactsComponent = null;
+
+    it('Null ContactsComponent Instance', () => {
+        expect(ContactsComponent).toBe(null);
+    })
+
     it('Should be a basic testing shell', () => {
-
         expect(true).toBe(true);
-
     });
 
 });
