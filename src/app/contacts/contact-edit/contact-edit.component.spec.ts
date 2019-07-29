3f063ed62e9e49779fe970d14390789bf7166711
Doug Franklin <doug@dougfranklin.com>
2019-07-29 05:48:04 -0400

    Second test done (invalid email)

--- src/app/contacts/contact-edit/contact-edit.component.spec.ts
+++ src/app/contacts/contact-edit/contact-edit.component.spec.ts
@@ -198,6 +198,37 @@ describe('ContactEditComponent', () => {
 
         }));
 
-        it('should not update the contact if email is invalid', fakeAsync(() => {}));
+        it('should not update the contact if email is invalid', fakeAsync(() => {
+
+            const newContact = {
+                id: 1,
+                name: 'london',          // 'chauncey' is expected here
+                email: 'london@example', // 'chauncey@example.com' is expected here
+                number: '1234567890'
+            };
+            
+            component.contact = {
+                id: 2,
+                name: 'chauncey',
+                email: 'chauncey@example.com',
+                number: '1234567890'
+            };
+            
+            component.isLoading = false;
+            fixture.detectChanges();
+
+            const nameInput = rootElement.query(By.css('.contact-name'));
+            
+            tick();
+
+            expect(nameInput.nativeElement.value).toBe('chauncey');
+            
+            component.updateContact(newContact);
+            fixture.detectChanges();
+            tick(100);
+
+            expect(nameInput.nativeElement.value).toBe('chauncey');
+
+        }));
     });
 });
